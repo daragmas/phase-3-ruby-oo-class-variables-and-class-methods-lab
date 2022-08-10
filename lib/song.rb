8ae5af291f48ff6d9f4414c28beb43a2ccbcc280
daragmas <daragmas@gmail.com>
2022-08-09 21:29:49 -0400

    commit tree b1ccac88f2f1fb93db00bf4499d383e354aa837f

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def initialize name, artist, genre
+        @@count+=1
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@genres << genre
+        @@artists << artist
+    end 
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+
+    def self.genres
+        @@genres.uniq
+    end
+
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+end
